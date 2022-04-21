73c970b781d24ffa036fcd95859e0f46c9d5f66f
David <David347luna@gmail.com>
2022-04-21 18:24:44 +0200

    Funciones acabadas

--- Practica 3/client.py
+++ Practica 3/client.py
@@ -1,31 +1,95 @@
 import json
+import datetime
 import requests
 
 def menu():
     print("1. Añadir sala\n2. Mostrar información de sala\n3. Añadir reserva\n4. Listar reservas\n5. Eliminar reserva\n6. Exit".format("utf-8"))
     
 def anadir_sala():
-    print("Introduce el ID de la sala: ")
-    roomId = int(input())
-    print("Introduce la capacidad de la sala: ")
-    capacity = int(input())
-    print("Introduce los recursos de la sala separado por un espacio")
-    resources = str(input()).split(" ")
-    response = requests.post("http://localhost:8080/addRoom",json = {"roomId":roomId,"capacity":capacity, "resources":resources})
+    usuario = iniciar_sesion()
+    roomId = int(input("Introduce el ID de la sala: "))
+    capacity = int(input("Introduce la capacidad de la sala: "))
+    resources = str(input("Introduce los recursos de la sala separado por un espacio: ")).split(" ")
+    response = requests.post("http://localhost:8080/addRoom",json = {"roomId":roomId,"capacity":capacity, "resources":resources, "userName":usuario[0], "password":usuario[1]})
+    print (response.text)
 
 def mostrar_info_sala():
-    print("Info mostrada")
+    usuario = iniciar_sesion()
+    idSala = int(input("Introduce el id de la sala: "))
+    response = requests.get("http://localhost:8080/showInformationRoom/{id}".format(id = idSala),json= {"userName": usuario[0],"password":usuario[1]})
+    print(response.text)
     
 def anadir_reserva():
-    print("Reserva anadida")
+    usuario = iniciar_sesion()
+    idSala = input("Introduce el id de la sala que quiere reservar: ")
+    Fecha = input("Introduce la fecha de la reserva formato DD/MM/AAAA: ")
+    try:
+        datetime.datetime.strptime(Fecha,"%d/%m/%Y")
+    except:
+        print("---- ERROR: FECHA EN FORMATO INCORRECTO ----\n")
+    startTime = input("Introduce la hora de inicio de la reserva, formato HORAS:MINUTOS : ")
+    try:
+        ts = datetime.datetime.strptime(startTime,"%H:%M")
+    except:
+        print("---- ERROR: HORA EN FORMATO INCORRECTO ----\n")
+        return
+    endTime = input("Introduce la hora del fin de la reserva, formato HORA:MINUTOS : ")
+    try:
+        te = datetime.datetime.strptime(endTime,"%H:%M")
+    except:
+        print("---- ERROR: HORA EN FORMATO INCORRECTO ----\n")
+        return
+    try:   
+        if (te <= ts):
+            raise 
+    except:
+        print("---- ERROR: Hora de finalización < Hora de entrada ----\n")
+        return
     
-def listar_reserva():
-    print("Lista de reservas")
+    send = {"userName":usuario[0],"password":usuario[1],"roomId":idSala,"date":Fecha,"startTime":startTime,"endTime":endTime}
+    response = requests.post("http://localhost:8080/addBooking",json=send)
+    print (response.text)
+    
+def listar_reservas():
+    usuario = iniciar_sesion()
+    userDNI = input("Introduce el DNI del usuario a consultar: ")
+    response = requests.get("http://localhost:8080/showBookings/{dni}".format(dni=userDNI),json={"userName": usuario[0],"password":usuario[1]})
+    print(response.text)
 
 def eliminar_reserva():
-    print("reserva Eliminada")
+    usuario = iniciar_sesion()
+    bookingId = input("Introduce la id de la reserva a eliminar: ")
+    response = requests.delete("http://localhost:8080/deleteBooking/{id}".format(id = bookingId),json={"userName": usuario[0],"password":usuario[1]})
+    print(response.text)
 
+    
+def iniciar_sesion():
+    usr_name = input("Introduce tu nombre de usuario: ")
+    password = input("Introduce tu contraseña: ")
+    return usr_name,password
+    
+
+def opcion_elegida(opcion):
+    if opcion == 1:
+        anadir_sala()
+    elif opcion == 2:
+        mostrar_info_sala()
+    elif opcion == 3:
+        anadir_reserva()
+    elif opcion == 4:
+        listar_reservas()
+    elif opcion == 5:
+        eliminar_reserva()
+    elif opcion == 6:
+        return 0
+    
+            
+                
 if __name__ == '__main__':
-    menu()
-    opcion = input()
-    anadir_sala()
+    while True:
+        menu()
+        opcion = int(input())
+        opcion_elegida(opcion)
+        if opcion == 6:
+            break
+    
--- Practica 3/server.py
+++ Practica 3/server.py
@@ -1,5 +1,6 @@
 import json
-from bottle import run, request, response, get, post, put
+import datetime
+from bottle import run, request, response, get, post, delete
 
 #* Cargamos el json en memoria
 with open('CommunicationJSON.json') as f:
@@ -18,7 +19,8 @@ class User:
         self.password = password
 
 class Booking:
-    def __init__(self,id,dni,date,startTime,endtime):
+    def __init__(self,roomId,id,dni,date,startTime,endtime):
+        self.roomId = roomId
         self.bookingId = id
         self.DNI = dni
         self.date = date
@@ -35,93 +37,176 @@ def carga():
         rooms.append(Room(index["roomId"],index["capacity"],index["resources"]))
 
     for index in database["users"]:
-        users.append(User(index["DNI"],index["userName"],["password"])) 
+        users.append(User(index["DNI"],index["userName"],index["password"])) 
     
     for index in database["booking"]:
-        bookinglist.append(Booking(index["bookingId"],index["DNI"],index["date"],index["startTime"],index["endTime"]))
+        bookinglist.append(Booking(index["roomId"],index["bookingId"],index["DNI"],index["date"],index["startTime"],index["endTime"]))
+
+
+def login_status(userName,password):
+    success = False
+    for index in users:
+        #print(str(index.userName) + " " + str(index.password))
+        if index.userName == userName and index.password == password:
+            success = True
+            break
+    return success
 
 #* Añadir Habitacion
 @post('/addRoom')
 def add_Room():
     data = request.json
-    roomId = data["roomId"]
-    capacity = data["capacity"]
-    resources = data["resources"]
-    for id in rooms:
-        if data["roomId"] == id.roomId:
-            raise ValueError
-    rooms.append(Room(roomId,capacity,resources))
-    dict_to_parse = {"roomId": roomId,"capacity": capacity,"resources":resources}
-    database["rooms"].append(dict_to_parse)
-    
-    with open ('CommunicationJSON.json','w') as f:
-        json.dump(database,f,indent=2)
+    if login_status(data["userName"],data["password"]):
+        roomId = data["roomId"]
+        capacity = data["capacity"]
+        resources = data["resources"]
+        for id in rooms:
+            if data["roomId"] == id.roomId:
+                raise ValueError
+        rooms.append(Room(roomId,capacity,resources))
+        dict_to_parse = {"roomId": roomId,"capacity": capacity,"resources":resources}
+        database["rooms"].append(dict_to_parse)
         
+        with open ('CommunicationJSON.json','w') as f:
+            json.dump(database,f,indent=2)
+        return "Sala añadida correctamente"
+    else:
+        return "Credenciales no Validas"
+        
+#Mostrar Informacion
 @get('/showInformationRoom/<id:int>')
 def show_Information_Room(id):
-    for index in rooms:
-        if index.roomId == id:
-            break
-    return json.dumps(index,indent = 2)
+    data = request.json
+    valido = False
+    if login_status(data["userName"],data["password"]):
+        for index in rooms:
+            if index.roomId == id:
+                formatData = {"roomId":index.roomId,"capacity":index.capacity,"resources":index.resources}
+                valido = True
+                break
+        if valido == True:
+            return json.dumps(formatData,indent = 2)
+        else:
+            return "ID de sala no valido"
+    else:
+        return "Credenciales no Validas"
 
 @post ('/addBooking')
 def add_Booking():
     data = request.json
-    lista = []
-    id_exist = False
-    for i in bookinglist:
-        if i.bookingId == data['bookingId']:
-            id_exist = True
-            if i.DNI != None:
-                error_Message = {'errorMessage':"La sala que desea reservar esta ocupada"}
+    if login_status(data["userName"],data["password"]):
+        for user in users:
+            if user.userName == data["userName"]:
+                DNI = user.DNI
+        free_room = True
+        room_exist = False
+        print (data["date"] +" " + data["startTime"])
+        startTime = datetime.datetime.strptime(data["date"] +" " + data["startTime"],"%d/%m/%Y %H:%M") 
+        endTime = datetime.datetime.strptime(data["date"] +" " + data["endTime"],"%d/%m/%Y %H:%M")
+        
+        for i in rooms:
+            if i.roomId == data["roomId"]:
+                room_exist = True
+                break
+        for i in bookinglist:
+            ts = datetime.datetime.strptime(i.date + " " + i.startTime,"%d/%m/%Y %H:%M")
+            te = datetime.datetime.strptime(i.date + " " + i.endTime,"%d/%m/%Y %H:%M")
+            checkTime = (startTime < ts and endTime < ts) or (startTime > te and endTime > te)
+            if i.roomId == data["roomId"]:
+                if not checkTime:
+                    free_room = False
+                else:
+                    room_exist = True
+                    break
+                    
+        
+                    
+        if free_room == False:
+            errorMsg = "La sala que desea reservar esta ocupada"
+            roomsinfo = []
+            for i in rooms:
+                has_booking = False
                 for j in bookinglist:
-                    if j.startTime == data['startTime'] and j.endTime == data["endTime"] and j.DNI == None:
-                        list.append(j)
-                error_Message["booking"] = list
-                return json.dumps(error_Message,indent = 2)
-        break
+                    if i.roomId == j.roomId:
+                        has_booking = True
+                        if j.date == data["date"]:
+                            ts = datetime.datetime.strptime(j.date + " " + j.startTime,"%d/%m/%Y %H:%M")
+                            te = datetime.datetime.strptime(j.date + " " + j.endTime,"%d/%m/%Y %H:%M")
+                            checkTime = (startTime < ts and endTime < ts) or (startTime > te and endTime > te)
+                            if checkTime:
+                                roomsinfo.append({"roomId":i.roomId,"capacity":i.capacity,"resources":i.resources})
                 
-    if id_exist == False:
-        return json.dumps(database["errorMessage"])
+                if not has_booking:
+                    roomsinfo.append({"roomId":i.roomId,"capacity":i.capacity,"resources":i.resources})
+                    
+                    
+                        
+            return json.dumps({"errorMessage":errorMsg,"rooms":roomsinfo})
+        elif free_room:
+            bookingId = bookinglist[len(bookinglist) - 1].bookingId + 1
+            roomId = data["roomId"]
+            date = data["date"]
+            start_Time = data["startTime"]
+            end_Time = data["endTime"]
+            
+            bookinglist.append(Booking(roomId,bookingId,DNI,date,start_Time,end_Time))
+            dict = {"bookingId":bookingId,"roomId":roomId,"DNI":DNI,"date":date,"startTime":start_Time,"endTime":end_Time}
+            database["booking"].append(dict)
+            
+            with open ('CommunicationJSON.json','w') as f:
+                json.dump(database,f,indent=2)
+            return json.dumps({"message": "Reserva anadida :-)"})
+        elif not room_exist:
+            return json.dumps({"errorMessage":"La sala no existe"})
     else:
-        index = bookinglist[bookinglist.index(i)]
-        i.DNI = data["DNI"]
-        i.startTime = data["startTime"]
-        i.endTime = data["endTime"]
-        bookinglist[index] = i
-        
-        database["booking"][index]["DNI"] = i.DNI
-        database["booking"][index]["startTime"] = i.startTime
-        database["booking"][index]["endTime"] = i.endTime
-        
-        with open ('CommunicationJSON.json','w') as f:
-            json.dump(database,f,indent=2)
+        return json.dumps({"errorMessage": "Credenciales no Validas"})
 
 @get('/showBookings/<userDni>')
 def show_Bookings(userDni):
-    list = []
-    for i in bookinglist:
-        if i.DNI == userDni:
-            list.append(i)
-    
-    dict = {"bookings":list}
-    return json.dumps(dict,indent = 2)
+    data = request.json
+    if login_status(data["userName"],data["password"]):
+        list = []
+        for i in bookinglist:
+            if i.DNI == userDni:
+                dict = {"bookingId":i.bookingId,"roomId":i.roomId,"DNI": i.DNI,"startTime":i.startTime,"endTime": i.endTime}
+                list.append(dict)
+        
+        dict = {"bookings":list}
+        
+        response.headers['Content-Type'] = 'application/json'
+        return json.dumps(dict,indent = 2)
+    else:
+        return json.dumps({"errorMessage":"Credenciales no Validas"})
 
-@post('deleteBooking/<bookingId>')
+@delete('/deleteBooking/<bookingId:int>')
 def delete_Booking(bookingId):
-    exist = False
-    for i in bookinglist:
-        if i.bookingId == bookingId:
-            exist = True
-            bookinglist.remove(i)
-            database['booking'].remove(i)
-            break
-    if exist == False:
-        error_Message = {"errorMessage":"No existe identificador de la reserva"}
-        return json.dumps(error_Message)
-    with open ('CommunicationJSON.json','w') as f:
-            json.dump(database,f,indent=2)
-    return {"Message":"Reserva eliminada"}
+    data = request.json
+    if login_status(data["userName"],data["password"]):
+        exist = False
+        for user in users:
+            if user.userName == data["userName"]:
+                DNI = user.DNI
+                break
+        for i in bookinglist:
+            if i.bookingId == bookingId:
+                if i.DNI != DNI:
+                    return json.dumps({"errorMessage": "No puedes eliminar una reserva que no te pertenece"})
+                else:
+                    exist = True
+                    index = bookinglist.index(i)
+                    bookinglist.remove(i)
+                    data_to_remove = {"booking"}
+                    del database['booking'][index]
+                    break
+        if exist == False:
+            error_Message = {"errorMessage":"No existe identificador de la reserva"}
+            return json.dumps(error_Message)
+        else:
+            with open ('CommunicationJSON.json','w') as f:
+                    json.dump(database,f,indent=2)
+        return {"Message":"Reserva eliminada"}
+    else:
+        return "Credenciales no Validas"
     
             
 if __name__ == '__main__':
